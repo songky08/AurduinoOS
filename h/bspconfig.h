4f0db4f0199f9ba1a2817ed2ab36bd42541523fb
songky08 <songky08@gmail.com>
2016-07-20 10:16:03 +0900

    init repository

---
+++ h/bspconfig.h
@@ -0,0 +1,17 @@
+/*
+ * bspconfig.h
+ *
+ *      Author: songky08@gmail.com
+ */
+
+#ifndef BSPCONFIG_H_
+#define BSPCONFIG_H_
+
+#include "tc27xc/IfxPort_reg.h"
+
+#define HIGH 	1
+#define LOW	 	0
+#define MAX_LED	54
+#define OUTPUT	0x10
+
+#endif /* BSPCONFIG_H_ */
---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *      Author: songky08@gmail.com
+ */
+
+#include "led.h"
+
+int main()
+{
+	pinMode(10, OUTPUT);
+	digitalWrite(10, HIGH);
+	return 0;
+}
