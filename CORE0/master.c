a47f56a03ddba67df0e7d6a0964b0587e1305f96
songky08 <songky08@gmail.com>
2016-09-09 20:01:31 +0900

    example of using tasks

--- CORE0/master.c
+++ CORE0/master.c
@@ -4,96 +4,74 @@
  *  Created on: 2016. 7. 28.
  *      Author: songky08@gmail.com
  */
+#include <machine/intrinsics.h>
 #include "led.h"
 #include "spi.h"
 #include "core.h"
-#include "interrupts.h"
+#include "usr_int.h"
+#include "task.h"
+#include "kernel.h"
 
-#define GPT_ISR_PRIO	2
-#define STM_ISR_PRIO	2
-#define SYSTIME_CLOCK	8	/* STM event rate [Hz] */
+extern void terminate(int tid);
+extern void mem_init(void);
 
-static Ifx_STM * const stm  = (Ifx_STM *)&MODULE_STM0;
-static Ifx_GPT12 * const gpt12 = (Ifx_GPT12 *)&MODULE_GPT120;
+_Bool state;
+int i;
 
-/* timer reload value (needed for subtick calculation) */
-static unsigned int reload_value = 0;
-_Bool stat = 0;
-
-/* timer interrupt routine */
-static void isrSTM(int reload_value)
+void task1()
 {
-	/* set new compare value */
-	stm->CMP[0].U += (unsigned int)reload_value;
+	static int i = 0;
 
-	stat = !stat;
+	digitalWrite(3, i = !i);
+
+	state = !state;
+
+	terminate(0);
 }
 
-static void isrGPT()
+void task2()
+{
+	static int i = 0;
+
+	digitalWrite(4, i = !i);
+
+	state = !state;
+
+	terminate(1);
+}
+
+void func()
+{
+	state = !state;
+}
+
+static void isrGPSR()
 {
 	//user code section
-}
+	_disable();
 
-static void initGPT(unsigned int hz)
-{
-	unsigned int frequency = SYSTEM_GetSysClock();
-	unsigned int reload_value = frequency / hz;
-
-	InterruptInstall(SRC_ID_GPT120T2, isrGPT, GPT_ISR_PRIO, (int)reload_value);
-
-	/* Enable Timer Mode */
-	gpt12->T3CON.B.T3M = 0;
-	/* Selects prescaler factor */
-	gpt12->T3CON.B.T3I = 0;
-	/* Timer T3 counts up */
-	gpt12->T3CON.B.T3UD = 0;
-	/* Enable Overflow/Underflow Output */
-	gpt12->T3CON.B.T3OE = 1;
-	/* Selects prescaler factor fgpt/8 */
-	gpt12->T3CON.B.BPS1 = 0;
-}
-static void initSTM(unsigned int hz)
-{
-	unsigned int frequency = SYSTEM_GetStmClock();
-
-	reload_value = frequency / hz;
-
-	InterruptInstall(SRC_ID_STM0SR0, isrSTM, STM_ISR_PRIO, (int)reload_value);
-
-	/* reset interrupt flag */
-	stm->ISCR.U = (IFX_STM_ISCR_CMP0IRR_MSK << IFX_STM_ISCR_CMP0IRR_OFF);
-	/* prepare compare register */
-	stm->CMP[0].U = stm->TIM0.U + reload_value;
-	stm->CMCON.B.MSIZE0 = 31;
-	stm->ICR.B.CMP0EN = 1;
+	_enable();
 }
 
 int master_run(int argc, char **argv)
 {
-	pinMode(3, OUTPUT);
-	InterruptInit();
-	/* start timer processing
-	 * initSTM function parameter configures frequency of interrupts
-	 * unit : hz */
-	initSTM(1);
-
 	pinMode(0, OUTPUT);
+	pinMode(3, OUTPUT);
+	pinMode(4, OUTPUT);
 	digitalWrite(0, HIGH);	//check core0 is running
 
-	serial_begin(9600);
-	serial_write("%d\n", 1231);
+	mem_init();
+	task_init();
+	task_create(task1);
+	task_create(task2);
 
-	while(1)
-	{
-		if(stat)
-		{
-			digitalWrite(3, HIGH);
-		}
-		else
-		{
-			digitalWrite(3, LOW);
-		}
-	}
+	InterruptInit();
+//	/* start timer processing */
+	initSTM0(1);
+	initSTM1(2);
+	initGPSR(0, 0, isrGPSR);
+
+	StartOS();
 
 	return 0;
 }
